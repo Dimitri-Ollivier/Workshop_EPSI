02a676cce058bb48aa21a610e28d79ee5cbc984e
Pinacolada14 <75010761+Pinacolada14@users.noreply.github.com>
2021-10-07 15:50:03 +0200

    Utilisation de la base dans la page Home

--- WorkshopB3/src/Controller/HomeController.php
+++ WorkshopB3/src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Emploi;
+use App\Entity\Evenement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,18 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'home')]
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Emploi::class);
+
+        $emplois = $repo->findAll();
+
+        $repo = $this->getDoctrine()->getRepository(Evenement::class);
+
+        $events = $repo->findAll();
 
         return $this->render('home/HomePage.html.twig', [
             'controller_name' => 'HomeController',
-
+            'emplois' => $emplois,
+            'events' => $events
         ]);
     }
 }
